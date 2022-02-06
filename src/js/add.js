bdec589bdf0501f5da97e54b508a84876a3a6547
Deepak Joy Jose <deejayjay@zoho.com>
2022-02-06 14:51:29 -0700

    Adds code to add a new toy.

--- src/js/add.js
+++ src/js/add.js
@@ -1,11 +1,65 @@
-// import { db, storage } from "./libs/firebase/firebaseConfig";
-// import { ref as storageRef, deleteObject } from "firebase/storage";
-// import { ref as databaseRef, get, remove } from "firebase/database";
+// Import the necessary firebase modules
+import { ref as dbRef, push, set } from "firebase/database";
+import { ref as storageRef, uploadBytes, getDownloadURL } from "firebase/storage";
+import { db, storage } from "./libs/firebase/firebaseConfig";
 
-// console.log(db, storage, storageRef, deleteObject, databaseRef, get, remove);
+const inputNewToyImage = document.getElementById("newToyImage");
 
+// Attach event listeners
 document.querySelector(".togglebtn-menu").addEventListener("click", onMenuButtonClick);
+inputNewToyImage.addEventListener("change", onToyImageSelected);
+document.forms["addToyForm"].addEventListener("submit", onAddNewToy);
 
+// Toggles menu when the menu button is clicked
 function onMenuButtonClick() {
   document.querySelector(".main-sidebar").classList.toggle("active-sidebar");
+}
+
+// Displays the selected image when a toy image is selected
+function onToyImageSelected(e) {
+  const file = e.target.files[0];
+  document.querySelector(".toy-image__display > img").src = URL.createObjectURL(file);
+}
+
+// Prevents the default form behaviour and saves the new toy info to firebase
+function onAddNewToy(e) {
+  e.preventDefault();
+
+  // Form data
+  const toyTitle = document.getElementById("toyName").value.trim();
+  const ageGroup = document.getElementById("toyAgeGroup").value.trim();
+  const price = document.getElementById("toyPrice").value.trim();
+  const toyImage = inputNewToyImage.files[0];
+
+  saveNewToyInfo(toyTitle, ageGroup, price, toyImage);
+}
+
+// Save the toy info to firebase
+async function saveNewToyInfo(title, ageGroup, price, image) {
+  // Path to the toys info on firebase Realtime Database
+  const dataRef = dbRef(db, "toys");
+
+  // Generate a firebase unique key for the new toy info
+  const itemRef = await push(dataRef);
+
+  // Path to the image file on firebase Storage
+  const imageRef = storageRef(storage, `images/twl${itemRef.key}`);
+
+  // Upload the image to the firebase storage
+  const uploadResult = await uploadBytes(imageRef, image);
+  const urlPath = await getDownloadURL(imageRef); // url to the image
+  const storagePath = uploadResult.metadata.fullPath; // Relative path to the image in the storage bucket
+
+  // Now, write the toy info to the realtime database under the path "toys"
+  set(itemRef, {
+    key: itemRef.key,
+    sku: `twl${itemRef.key}`,
+    image: urlPath,
+    imageStoragePath: storagePath,
+    title,
+    price,
+    ageGroup
+  })
+    .then(() => window.location.assign("index.html"))
+    .catch(err => console.log(`Unexpected error occurred while saving new toy info: ${err.message}`));
 }
